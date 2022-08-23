efe0643c8d33b544416bc28d48c42b79797963e1
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-08-23 19:21:53 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div className="app"></div>;
+  }
+}
+
+export default App;
