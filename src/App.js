236cac1e5d48daeca1b96b869c2e21da295890d3
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-08-25 19:53:53 +0300

    convert App class to function

--- src/App.js
+++ src/App.js
@@ -1,63 +1,58 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
 import FormContainer from './components/FormContainer';
 import Results from './components/Results';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // results: [],
-      name: '',
-      email: '',
-      phone: '',
-      schoolName: '',
-      titleOfStudy: '',
-      startDate: '',
-      endDate: '',
-      companyName: '',
-      position: '',
-      tasks: '',
-      jobStartDate: '',
-      jobEndDate: '',
-
-      renderStatus: false,
-      disabledStatus: false,
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-  }
-  handleChange = (e) => {
+const App = (props) => {
+  const [state, setState] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    schoolName: '',
+    titleOfStudy: '',
+    startDate: '',
+    endDate: '',
+    companyName: '',
+    position: '',
+    tasks: '',
+    jobStartDate: '',
+    jobEndDate: '',
+    renderStatus: false,
+    disabledStatus: false,
+  });
+  const handleChange = (e) => {
     const key = e.target.name;
-    this.setState({
+    setState((state) => ({
+      ...state,
       [key]: e.target.value,
-    });
+    }));
   };
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    this.setState({
+    setState((state) => ({
+      ...state,
       renderStatus: true,
       disabledStatus: true,
-    });
+    }));
 
     document.querySelector('#results').hidden = false;
   };
-  handleEdit = () => {
+  const handleEdit = () => {
     const values = document.querySelector('#form');
     //console.log(values.name.value);
-    for (const property in this.state) {
+    for (const property in state) {
       if (property !== 'renderStatus' && property !== 'disabledStatus')
-        values[property].value = this.state[property];
+        values[property].value = state[property];
     }
     document.querySelector('#results').hidden = true;
-    this.setState({
+    setState((state) => ({
+      ...state,
       disabledStatus: false,
-    });
+    }));
   };
-  render() {
-    /*if (this.state.renderStatus) {
+
+  /*if (this.state.renderStatus) {
       return (
         <div>
           <FormContainer
@@ -78,17 +73,16 @@ class App extends Component {
       );
     }
   }*/
-    return (
-      <div>
-        <FormContainer
-          handleSubmit={this.handleSubmit}
-          handleChange={this.handleChange}
-          disabledStatus={this.state.disabledStatus}
-        />
-        <Results results={this.state} handleEdit={this.handleEdit} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <FormContainer
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        disabledStatus={state.disabledStatus}
+      />
+      <Results results={state} handleEdit={handleEdit} />
+    </div>
+  );
+};
 
 export default App;
